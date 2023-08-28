81a4afb9e6f7335a917b0b59dfdda1b371cc1a04
zackdaspy <zahedajafari@gmail.com>
2023-08-28 14:08:33 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  //   Write your code here.
+  <div className="container">
+    <h1>Super Over League</h1>
+    <div>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
